22236cfffe2e8a40852ebad518ac12b20ff9517b
Aron Jones <aron.jones@gmail.com>
2014-06-23 11:48:22 -0400

    add support for django jinja

--- bootstrap_paginator/templatetags/paginator.py
+++ bootstrap_paginator/templatetags/paginator.py
@@ -3,6 +3,17 @@ from django.utils.six.moves.urllib.parse import urlencode
 from django import template
 from django.conf import settings
 
+try:
+    from django_jinja import library
+    lib = library.Library()
+
+except ImportError:
+    class LibraryStub(object):
+        @staticmethod
+        def global_function(func):
+            return func
+    lib = LibraryStub()
+
 
 PAGINATOR_ADJACENT_PAGES = getattr(settings, 'PAGINATOR_ADJACENT_PAGES', 2)
 
@@ -22,13 +33,13 @@ def paginator(context, page=None):
     page = context.get('page_obj', page)
     paginator = page.paginator
 
-    startPage = page.number - PAGINATOR_ADJACENT_PAGES
-    if startPage <= PAGINATOR_ADJACENT_PAGES + 1:
-        startPage = 1
-    endPage = page.number + PAGINATOR_ADJACENT_PAGES + 1
+    start_page = page.number - PAGINATOR_ADJACENT_PAGES
+    if start_page <= PAGINATOR_ADJACENT_PAGES + 1:
+        start_page = 1
+    end_page = page.number + PAGINATOR_ADJACENT_PAGES + 1
 
     page_numbers = [
-        n for n in range(startPage, endPage)
+        n for n in range(start_page, end_page)
         if n >= 1 and n <= paginator.num_pages
     ]
 
@@ -50,3 +61,10 @@ def append_to_get(context, **kwargs):
         get = {}
     get.update(kwargs)
     return '?{}'.format(urlencode(get))
+
+
+@lib.global_function
+def urlencode_plus(values, **plus):
+    values = values.copy()
+    values.update(plus)
+    return '?{}'.format(urlencode(values))
